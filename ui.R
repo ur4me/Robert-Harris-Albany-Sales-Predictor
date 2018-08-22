0b854071a5cdd27ddb455858456a557e033e8857
Steve Kim <30827345+ur4me@users.noreply.github.com>
2018-08-23 10:14:04 +1200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,36 +1,48 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+#preparation
 library(shiny)
-library(plotly)
+library(ggplot2)
+library(dplyr)
+library(ranger)
+library(lubridate)
+library(xgboost)
 
-ui <- fluidPage(
-  titlePanel("Iris Species Predictor"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        inputId = "petal.length",
-        label = "Petal Length (cm)",
-        min = 1,
-        max = 7,
-        value = 4),
-      sliderInput(
-        inputId = "petal.width",
-        label = "Petal Width (cm)",
-        min = 0.0,
-        max = 2.5,
-        step = 0.5,
-        value = 1.5),
-      p("To predict a species of iris flower, please select the petal length and the petal width using the two sliders above."),
-      p("The predicted species will be displayed in the text on the top right. In addition, a data visualization in the bottom right will show how the decision was made. The lines represent the decision boundaries and the red X represents your selected input.")),
-    mainPanel(
-      textOutput(
-        outputId = "text"),
-      plotOutput(
-        outputId = "plot"))))
+total <- read.csv('data/total.csv', na.strings = c("", "NA"),stringsAsFactors = T)
+
+load("model/rf.rda")
+load("model/lm_total.rda")
+load("model/lm_seasonal.rda")
+load("model/lm_weather.rda")
+
+total$Travel.Date <- as.Date(total$Travel.Date)
+airport <- total %>% group_by(month) %>% summarise(bookings = mean(bookings, na.rm = T ))
+total$month <- as.factor(total$month)
+
+#UI
+ui <- pageWithSidebar(
+  headerPanel("Robert Harris Albany Sales Predictor"),
+  sidebarPanel(
+
+    fluidPage(
+      
+      # Copy the line below to make a date selector 
+      dateInput("date", label = h3("Select Date"), value = Sys.Date(), min = min(total$Travel.Date) , max = max(total$Travel.Date) ),
+      
+      hr(),
+      fluidRow(column(3, verbatimTextOutput("value")))
+      
+    )
+    
+  ),
+  mainPanel(
+    h3("Estimated Sales per Day:"),
+    h4("(Modified data was used for information protection)"),
+    p("with linear regression model"),
+    h2(verbatimTextOutput("results_lm")),
+    
+    p("with Extreme Gradient Boosting model"),
+    h2(verbatimTextOutput("results_rf")),
+    
+    plotOutput("icg.plot1"),
+    style = "position:fixed;right:1px;")
+  
+)
